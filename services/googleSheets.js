e9ab32e54f3c19ea0339006062004fa09ba3963e
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-23 21:04:23 +0800

    Update googleSheets.js

--- services/googleSheets.js
+++ services/googleSheets.js
@@ -1,18 +1,43 @@
 import { google } from "googleapis";
 
-export async function getSheetValues(spreadsheetId, range) {
+// Authenticate Google Sheets
+async function getSheetsClient() {
   const auth = new google.auth.GoogleAuth({
     credentials: JSON.parse(process.env.GOOGLE_SERVICE_KEY),
     scopes: ["https://www.googleapis.com/auth/spreadsheets"],
   });
 
   const client = await auth.getClient();
-  const sheets = google.sheets({ version: "v4", auth: client });
+  return google.sheets({ version: "v4", auth: client });
+}
 
-  const response = await sheets.spreadsheets.values.get({
+// Read data from a sheet
+export async function readRange(spreadsheetId, range) {
+  const sheets = await getSheetsClient();
+
+  const res = await sheets.spreadsheets.values.get({
     spreadsheetId,
     range,
   });
 
-  return response.data.values;
+  return res.data.values || [];
+}
+
+// Write data to a sheet
+export async function writeRange(spreadsheetId, range, values) {
+  const sheets = await getSheetsClient();
+
+  const res = await sheets.spreadsheets.values.update({
+    spreadsheetId,
+    range,
+    valueInputOption: "RAW",
+    requestBody: { values },
+  });
+
+  return res.data;
+}
+
+// Optional: if you still need this
+export async function getSheetValues(spreadsheetId, range) {
+  return readRange(spreadsheetId, range);
 }
