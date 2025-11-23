538abced374d4189fd468fae65f0a823b3024ad2
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-23 21:01:00 +0800

    Create googleSheets.js

---
+++ services/googleSheets.js
@@ -0,0 +1,18 @@
+import { google } from "googleapis";
+
+export async function getSheetValues(spreadsheetId, range) {
+  const auth = new google.auth.GoogleAuth({
+    credentials: JSON.parse(process.env.GOOGLE_SERVICE_KEY),
+    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+  });
+
+  const client = await auth.getClient();
+  const sheets = google.sheets({ version: "v4", auth: client });
+
+  const response = await sheets.spreadsheets.values.get({
+    spreadsheetId,
+    range,
+  });
+
+  return response.data.values;
+}
