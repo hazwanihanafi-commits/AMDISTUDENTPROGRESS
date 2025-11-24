bfd57cf41a3c5974fb99c7993f0cda265d2c6c5a
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-25 06:16:26 +0800

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,10 @@
+// routes/api.js
 import express from "express";
-import indexRouter from "./routes/index.js";
-import apiRouter from "./routes/api.js";
-import studentRouter from "./routes/student.js";
 
-const app = express();
+const router = express.Router();
 
-app.set("views", "./views");
-app.set("view engine", "ejs");
+router.get("/", (req, res) => {
+  res.json({ ok: true, message: "API is working" });
+});
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
-
-// ROUTES
-app.use("/", indexRouter);
-app.use("/api", apiRouter);
-app.use("/student", studentRouter);
-
-export default app;
+export default router;
