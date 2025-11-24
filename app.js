b5d0495d6a597272ae8c3bd84e90733bf32d4f93
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-25 07:38:54 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
 import express from "express";
-import authRouter from "./routes/auth.js";
 import indexRouter from "./routes/index.js";
 import apiRouter from "./routes/api.js";
 import studentRouter from "./routes/student.js";
+import authRouter from "./routes/auth.js";
+
 
 const app = express();
-
 app.set("views", "./views");
 app.set("view engine", "ejs");
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.use("/", authRouter);
+
+// Register routes
+app.use("/", authRouter); // login routes first
 app.use("/", indexRouter);
-app.use("/api", apiRouter);       // <-- VERY IMPORTANT
+app.use("/api", apiRouter);
 app.use("/student", studentRouter);
 
-app.use((req, res) => {
-  res.status(404).send("Page Not Found");
-});
+
+// 404
+app.use((req, res) => res.status(404).send('Page Not Found'));
+
 
 export default app;
