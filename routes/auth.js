cde250755cbfe79f27ed3b5e0eb1f0795cdc411f
hazwanihanafi-commits <hazwanihanafi@gmail.com>
2025-11-25 07:29:38 +0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,13 @@
 import express from "express";
 const router = express.Router();
 
-// Show login page
 router.get("/login", (req, res) => {
   res.sendFile("login.html", { root: "./public" });
 });
 
-// Process login
 router.post("/login", (req, res) => {
   const { username, password } = req.body;
 
-  // Simple fixed login
   if (username === "admin" && password === "admin123") {
     return res.redirect("/admin");
   }
